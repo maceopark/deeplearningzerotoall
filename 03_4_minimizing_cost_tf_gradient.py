b7270687694286684d3cf1e560aed1d5f93f2610
Nochul Park <maceo.park@gmail.com>
2017-12-07 23:24:52 -0800

    mvar linear regression

--- 03_4_minimizing_cost_tf_gradient.py
+++ 03_4_minimizing_cost_tf_gradient.py
@@ -5,19 +5,24 @@ X = [1, 2, 3]
 Y = [1, 2, 3]
 
 # Set wrong model weight
-W = tf.Variable(-3.0)
+W = tf.Variable(5.0)
 
 # Linear model
 hypothesis = X * W
+# Manial gradient
+gradient = tf.reduce_mean((W*X-Y)*X)*2
 # cost function
 cost = tf.reduce_mean(tf.square(hypothesis - Y))
 # Minimize: Gradient Descent Magic
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
-train = optimizer.minimize(cost)
+
+# Get gradients
+gvs = optimizer.compute_gradients(cost)
+apply_gradients = optimizer.apply_gradients(gvs)
 
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
 for step in range(100) :
-    print(step, sess.run(W))
-    sess.run(train)
+    print(step, sess.run([gradient, W, gvs]))
+    sess.run(apply_gradients)
